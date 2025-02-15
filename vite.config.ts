89779d723a885d0db6f1841db10751eae0a76ec2
daichee <d.kondo2618@gmail.com>
2025-02-15 23:25:45 +0900

    for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-todo-app/'
+  base: '/react-todo-app/',  // Vercelのデプロイパスに合わせる
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
