85dc826cd37bb3ee07c658ae2a3aa82feec2f8a1
daichee <d.kondo2618@gmail.com>
2025-02-16 00:08:56 +0900

    fixed new todo button

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App: React.FC = () => {
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <Header onNewTodo={() => setIsFormOpen(true)} />
+            <Header onCreateClick={() => setIsFormOpen(true)} />
 
             <main className="container mx-auto px-4 py-8">
                 <FilterSection
